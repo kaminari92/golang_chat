b53efcf10cc0714dc39f00be547cea7b22a96ecf
kaminari92 <102847605+kaminari92@users.noreply.github.com>
2022-06-03 17:19:38 -0600

    GOLANG - GoChat First Commit

---
+++ cmd/web/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := routes()
+	log.Println("Starting web server on port 8081")
+	_ = http.ListenAndServe(":8081", mux)
+}
---
+++ internal/handlers/handlers.go
@@ -0,0 +1,37 @@
+package handlers
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/CloudyKit/jet/v6"
+)
+
+var views = jet.NewSet(
+	jet.NewOSFileSystemLoader("./html"),
+	jet.InDevelopmentMode(),
+)
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	err := renderPage(w, "home.html", nil)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func renderPage(w http.ResponseWriter, tmp string, data jet.VarMap) error {
+	view, err := views.GetTemplate(tmp)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	err = view.Execute(w, data, nil)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	return nil
+
+}
