e7921256e1ff31bba87682059ec321dee2ae481a
kaminari92 <wgrijalva.git@gmail.com>
2022-06-03 22:16:40 -0600

    GOLANG - GoChat : improving our WS endpoint c.9

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"gochat/internal/handlers"
 	"log"
 	"net/http"
 )
 
 func main() {
 	mux := routes()
+	log.Println("Starting channel listener")
+	go handlers.ListenToWsChannel()
 	log.Println("Starting web server on port 8081")
 	_ = http.ListenAndServe(":8081", mux)
 }
